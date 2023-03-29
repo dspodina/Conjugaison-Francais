52ccbcee8e708d8f1a0baf7306b38974a54ce01b
Daria <spodina.darya@gmail.com>
2023-03-29 20:25:30 +0200

    Changed style for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="bg-greenish h-[70px] text-lg lg:flex">
-      <div className="container mx-auto  flex items-center">
+    <div className="bg-greenish h-[70px] text-lg flex items-center">
+      <div className="container mx-auto flex items-center ml-8">
         <p>©2023 - Pa-ra-pa-parler. All rights reserved.</p>
       </div>
     </div>
